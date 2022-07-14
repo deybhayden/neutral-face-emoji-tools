e84ae37802c1d68f4451d17f31f44491b9791eb7
Ben Hayden <hayden767@gmail.com>
2022-07-14 10:09:10 -0500

    Update packages, switch CSS plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,34 @@
-import svelte from 'rollup-plugin-svelte';
-import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import globals from 'rollup-plugin-node-globals';
-import builtins from 'rollup-plugin-node-builtins';
-import embedCSS from 'rollup-plugin-embed-css';
+import svelte from "rollup-plugin-svelte";
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import globals from "rollup-plugin-node-globals";
+import builtins from "rollup-plugin-node-builtins";
+import css from "rollup-plugin-import-css";
 
 const config = {
-  input: 'src/content.js',
+  input: "src/content.js",
   output: {
-    file: 'dist/content.js',
-    format: 'iife'
+    file: "dist/content.js",
+    format: "iife",
   },
   plugins: [
-    embedCSS(),
+    css(),
     svelte({
-      dev: true
+      emitCss: true,
     }),
     babel({
-      babelHelpers: 'bundled'
+      babelHelpers: "bundled",
     }),
     resolve({
-			browser: true,
-			dedupe: ['svelte'],
-			preferBuiltins: true,
-		}),
-		commonjs(),
-		globals(),
-		builtins()
-  ]
+      browser: true,
+      dedupe: ["svelte"],
+      preferBuiltins: true,
+    }),
+    commonjs(),
+    globals(),
+    builtins(),
+  ],
 };
 
 export default config;
